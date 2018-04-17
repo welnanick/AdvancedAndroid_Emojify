5cbdb850ca1b35c2da7aec678af3b95a3055ebc3
Nicholas Welna <welnanick@gmail.com>
2018-04-17 10:37:06 -0700

    TLIB.02-Exercise-DetectFaces: Finished

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,31 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+public class Emojifier {
+
+    public static void detectFaces(Context context, Bitmap bitmap) {
+
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        SparseArray<Face> faces = detector.detect(frame);
+
+        Log.d("Emojifier", faces.size() + " faces found");
+
+        detector.release();
+
+    }
+
+}
